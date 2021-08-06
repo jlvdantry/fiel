6cf311694e5442188b3c7f602246455969ba2281
José Luis Vasquez <jlvdantry@hotmail.com>
2021-08-06 11:46:58 -0500

    poner justify a los textos de la ayuda

--- src/componente/Ayuda.js
+++ src/componente/Ayuda.js
@@ -24,7 +24,6 @@ class Ayuda extends Component {
         }
   render() {
      const { collapseID } = this.state;
-     console.log('rendereo '+collapseID);
      return  (
         <div id="ayuda" data-role="dialog" data-url="ayuda" data-theme="d">
           <div data-role="header">
@@ -36,7 +35,7 @@ class Ayuda extends Component {
 		      <UncontrolledCollapse toggler="#toggler" isOpen={collapseID==='toggler' ? true : false} toggleEvents={['touchstart']} >
 			<Card >
 			  <CardBody>
-				  <p>La <b>FIEL</b> también es conocida como la e.firma o firma electrónica y consta de dos archivos y una contraseña, uno de los archivos viene en formato .cer (certificado) llamada llave pública y el otro archivo en formato .key llamada llave privada. La <b>FIEL</b> permite generar sellos electrónicos de las facturas que se emitan. Este sello electrónico tiene la misma validez que la firma autógrafa. </p>
+				  <p className="text-justify">La <b>FIEL</b> también es conocida como la e.firma o firma electrónica y consta de dos archivos y una contraseña, uno de los archivos viene en formato .cer (certificado) llamada llave pública y el otro archivo en formato .key llamada llave privada. La <b>FIEL</b> permite generar sellos electrónicos de las facturas que se emitan. Este sello electrónico tiene la misma validez que la firma autógrafa. </p>
 			  </CardBody>
 			</Card>
 		      </UncontrolledCollapse>
@@ -46,9 +45,9 @@ class Ayuda extends Component {
 		      <UncontrolledCollapse toggler="#toggler1"  isOpen={collapseID==='toggler1' ? true : false}>
                         <Card>
                           <CardBody>
-                  <p><b className="text-info">1.</b> Este aplicativo le permite validar la llave privada y pública generada por el <b>SAT</b>. Teniendo como principal virtud que la llave privada y pública <b>no</b> viajan por internet. Esto incrementa la seguridad de sus datos confidencianciales, previniendo ser víctima de un ciber crimen al no exponer sus llaves y evitar que su firma electrónica pueda ser <b className="text-alert">hackeada</b></p>
-                  <p><b className="text-info">2.</b> Poder validar las facturas electrónicas emitidas con los formatos establecido por el SAT. En caso contrario indicará que la factura no es válida.</p>
-                  <p><b className="text-info">3.</b> Firmar electrónicamente cualquier conjunto de datos. A este aplicativo se le pueden adaptar servicios de datos externos para obtener información, firmarlos electrónicamente y generar el sello electrónico.</p>
+                  <p className="text-justify"><b className="text-info">1.</b> Este aplicativo le permite validar la llave privada y pública generada por el <b>SAT</b>. Teniendo como principal virtud que la llave privada y pública <b>no</b> viajan por internet. Esto incrementa la seguridad de sus datos confidencianciales, previniendo ser víctima de un ciber crimen al no exponer sus llaves y evitar que su firma electrónica pueda ser <b className="text-alert">hackeada</b></p>
+                  <p className="text-justify"><b className="text-info">2.</b> Poder validar las facturas electrónicas emitidas con los formatos establecido por el SAT. En caso contrario indicará que la factura no es válida.</p>
+                  <p className="text-justify"><b className="text-info">3.</b> Firmar electrónicamente cualquier conjunto de datos. A este aplicativo se le pueden adaptar servicios de datos externos para obtener información, firmarlos electrónicamente y generar el sello electrónico.</p>
                           </CardBody>
                         </Card>
                       </UncontrolledCollapse>
@@ -59,8 +58,8 @@ class Ayuda extends Component {
                       <UncontrolledCollapse toggler="#toggler2" isOpen={collapseID==='toggler2' ? true : false}>
                         <Card>
                           <CardBody>
-                  <p><b className="text-info">1.</b> Contar con la llave privada y pública las cuales deberán de estar almacenadas en el móvil o computador.</p>
-                  <p><b className="text-info">2.</b> Conocer la contraseña de la llave privada.</p>
+                  <p className="text-justify"><b className="text-info">1.</b> Contar con la llave privada y pública las cuales deberán de estar almacenadas en el móvil o computador.</p>
+                  <p className="text-justify"><b className="text-info">2.</b> Conocer la contraseña de la llave privada.</p>
                           </CardBody>
                         </Card>
                       </UncontrolledCollapse>
@@ -71,11 +70,11 @@ class Ayuda extends Component {
                       <UncontrolledCollapse toggler="#toggler3" isOpen={collapseID==='toggler3' ? true : false}>
                         <Card>
                           <CardBody>
-                  <p><b className="text-info">1.</b> Dar un clic en la opción <b className="text-info">"MI <b>FIEL</b>"</b>. Aquí el aplicativo le mostrará tres botones dos de ellos son para ubicar la <b>FIEL</b> y el tercero para validar la llave privada contra su contraseña.</p>
-                  <p><b className="text-info">2.</b> Dar clic en el boton <b className="text-info">"Ubicar llave pública"</b>. Aquí el aplicativo le solicitará la ubicación del certificado que es un archivo con extensión cer .</p>
-                  <p><b className="text-info">3.</b> Dar clic en el boton <b className="text-info">"Ubicar llave privada"</b>. Aquí el aplicativo le solicitará la ubicación de la llave privada que es un archivo con extensión key .</p>
-                  <p><b className="text-info">4.</b> Una vez que ya cargo la <b>FIEL</b>, se debe de teclear la contraseña de la llave privada y posteriormente dar clic en el botón <b className="text-info">"Validar"</b>. Si todo esta correcto el aplicativo le indicara que la <b>FIEL</b> y la contraseña checan entre si, caso contrario indicara que no checa la <b>FIEL</b>.</p>
-                  <p><b className="text-info">Nota</b> Si la <b>FIEL</b> es correcta, se esta en la posibilidad de generar sellos electrónicos no solo de factura electrónicas si no de cualquier conjunto  de datos como por ejemplo contratos.</p>
+                  <p className="text-justify"><b className="text-info">1.</b> Dar un clic en la opción <b className="text-info">"MI <b>FIEL</b>"</b>. Aquí el aplicativo le mostrará tres botones dos de ellos son para ubicar la <b>FIEL</b> y el tercero para validar la llave privada contra su contraseña.</p>
+                  <p className="text-justify"><b className="text-info">2.</b> Dar clic en el boton <b className="text-info">"Ubicar llave pública"</b>. Aquí el aplicativo le solicitará la ubicación del certificado que es un archivo con extensión cer .</p>
+                  <p className="text-justify"><b className="text-info">3.</b> Dar clic en el boton <b className="text-info">"Ubicar llave privada"</b>. Aquí el aplicativo le solicitará la ubicación de la llave privada que es un archivo con extensión key .</p>
+                  <p className="text-justify"><b className="text-info">4.</b> Una vez que ya cargo la <b>FIEL</b>, se debe de teclear la contraseña de la llave privada y posteriormente dar clic en el botón <b className="text-info">"Validar"</b>. Si todo esta correcto el aplicativo le indicara que la <b>FIEL</b> y la contraseña checan entre si, caso contrario indicara que no checa la <b>FIEL</b>.</p>
+                  <p className="text-justify"><b className="text-info">Nota</b> Si la <b>FIEL</b> es correcta, se esta en la posibilidad de generar sellos electrónicos no solo de factura electrónicas si no de cualquier conjunto  de datos como por ejemplo contratos.</p>
                           </CardBody>
                         </Card>
                       </UncontrolledCollapse>
@@ -87,9 +86,9 @@ class Ayuda extends Component {
                         <Card>
                           <CardBody>
                   <p><b className="text-info">1.</b> Dar un clic en la opción <b className="text-info">"MIS FACTURAS"</b>. Aquí el aplicativo le mostrará tres botones que son <b className="text-info">"Ubicar factura"</b> , <b className="text-info">"Validar factura"</b> y pór último  <b className="text-info">"Consultar factura"</b></p>
-                  <p><b className="text-info">2.</b> Ubicar la factura electrónica dando un clic en el botón <b className="text-info">"Ubicar factura"</b> este tipo de archivo deben de tener extensión xml.</p>
-                  <p><b className="text-info">3.</b> Dar clic en el botón <b className="text-info">"Validar factura"</b> , si la factura electrónica es válida desplegara el contenido de este caso y teniendo la opción de poder agregarla a un historico de facturas para posteriormente consultarla o consolidar la información en caso contrario enviara el mensaje de que la factura electrónica no es valida.</p>
-                  <p><b className="text-info">4.</b> Para consultar el historial solo hay que dar un clic en el botón <b className="text-info">"Consultar historial"</b> El cual le mostrara todas las factura que fueron agregadas al historial.</p>
+                  <p className="text-justify"><b className="text-info">2.</b> Ubicar la factura electrónica dando un clic en el botón <b className="text-info">"Ubicar factura"</b> este tipo de archivo deben de tener extensión xml.</p>
+                  <p className="text-justify"><b className="text-info">3.</b> Dar clic en el botón <b className="text-info">"Validar factura"</b> , si la factura electrónica es válida desplegara el contenido de este caso y teniendo la opción de poder agregarla a un historico de facturas para posteriormente consultarla o consolidar la información en caso contrario enviara el mensaje de que la factura electrónica no es valida.</p>
+                  <p className="text-justify"><b className="text-info">4.</b> Para consultar el historial solo hay que dar un clic en el botón <b className="text-info">"Consultar historial"</b> El cual le mostrara todas las factura que fueron agregadas al historial.</p>
                           </CardBody>
                         </Card>
                       </UncontrolledCollapse>
@@ -100,9 +99,9 @@ class Ayuda extends Component {
                       <UncontrolledCollapse toggler="#toggler5" isOpen={collapseID==='toggler5' ? true : false}>
                         <Card>
                           <CardBody>
-                  <p><b className="text-info">1.</b> Para poder firmar electrónicamente un conjunto de datos la <b>FIEL</b> debio de haber sido validada previamente.</p>
-                  <p>Este aplicativo puede adaptarse para comunicarse con otros sistemas, con el objetivo de obtener el conjunto de datos a firmar electrónicamente. Y una vez generado el sello digital correspondiente, este se puede enviar al sistema que solicito el <b>firmado electrónico</b>.</p>
-                  <p>Se recomienda que los datos a firmar se encuentren en formato JSON.</p>
+                  <p className="text-justify"><b className="text-info">1.</b> Para poder firmar electrónicamente un conjunto de datos la <b>FIEL</b> debio de haber sido validada previamente.</p>
+                  <p className="text-justify">Este aplicativo puede adaptarse para comunicarse con otros sistemas, con el objetivo de obtener el conjunto de datos a firmar electrónicamente. Y una vez generado el sello digital correspondiente, este se puede enviar al sistema que solicito el <b>firmado electrónico</b>.</p>
+                  <p className="text-justify">Se recomienda que los datos a firmar se encuentren en formato JSON.</p>
                           </CardBody>
                         </Card>
                       </UncontrolledCollapse>
