5b7d904560bb2779c59639257c8973aedb7c32d4
Jose Luis Vasquez Barbosa <jlvdantry@hotmail.com>
2024-08-29 13:54:40 -0600

    se corrigio problema de constantes

--- build/Constantes.js
+++ build/Constantes.js
@@ -2,6 +2,7 @@ var  TC = { I:'Ingreso', E:'Egreso',N:'Nomina',P:'Pagos',T:'Trapaso' };
 var  MENUS = { DESCARGAMASIVA:'1'  };
 var  FORMA = { DESCARGAMASIVA:'1'  };
 var  MOVIMIENTO = { AUTENTICA:'1',SOLICITUD:'2',VERIFICA:'3',DESCARGA:'4'  };
-var  ESTADOREQ = { INICIAL:0, AUTENTICADO:10 , RESPUESTADESCONOCIDA:99, RECIBIDO:1,ERROR:500,TOKENINVALIDO:300};  /* Estado del requerimiento */
-var  REVISA = { VIGENCIATOKEN : 60 }; // segundos
-export { TC,MENUS,FORMA,MOVIMIENTO,ESTADOREQ,REVISA }
+var  ESTADOREQ = { INICIAL:0, AUTENTICADO:10 , RESPUESTADESCONOCIDA:99, RECIBIDO:1,ERROR:500,ACEPTADO:5000,REQUIRIENDO:2,TOKENINVALIDO:300};  /* Estado del requerimiento */
+var  TOKEN = { TIMELIVE : 5 }   // tiempo en que vigente el token proporcionado por el SAT, para controlar si esta aun viente el token
+var  REVISA = { VIGENCIATOKEN : 60, ESTADOREQ:20 }; // segundos
+var  PWDFIEL = null;
