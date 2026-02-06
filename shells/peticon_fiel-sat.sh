d662425fbd19cd49b8486e54380fd4b131942abd
Jose Luis Vasquez Barbosa <jlvdantry@hotmail.com>
2026-02-05 19:29:30 -0600

    se agrega shell para probar el login con la fiel

---
+++ shells/peticon_fiel-sat.sh
@@ -0,0 +1,5 @@
+curl -v -k -X POST https://fiel-sat.so-lu-int.com/api/loginfiel \
+-H "Content-Type: application/json" \
+-H "Origin: https://fiel-dev.so-lu-int.com" \
+-H "Accept: application/json" \
+-d '{"test":"enviando_datos_para_probar_limite_de_apache"}'
